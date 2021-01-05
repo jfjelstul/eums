0d947c15a39e415a1d84e24876741dd2f46abbf2
Josh Fjelstul <joshfjelstul@gmail.com>
2021-01-05 16:35:10 -0600

    Update replication.R

--- replication-code/replication.R
+++ replication-code/replication.R
@@ -1,10 +1,10 @@
 ###########################################################################
 # Joshua C. Fjelstul, Ph.D.
-# eutris R package
+# eumem R package
 ###########################################################################
 
 # read in data
-member_states <- read.csv("data-raw/member_states.csv", stringsAsFactors = FALSE)
+member_states <- read.csv("data-raw/member-states.csv", stringsAsFactors = FALSE)
 
 # save
 save(member_states, file = "data/member_states.RData")
